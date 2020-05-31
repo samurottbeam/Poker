10945a2728ec3809f0f362878e64293c2f36f57d
jason-vu <66186269+jason-vu@users.noreply.github.com>
2020-05-30 20:28:24 -0700

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -65,4 +65,89 @@ public class Hand{
 	public void organizeHand(){
 		Collections.sort(h);
 	}
+
+	//returns int representing value of the hand: 1 for high card, 9 for straight flush
+	public int handValue (){
+		if (isStraight() && isFlush()) {
+			return 9;
+		} else if (isFourOfKind()){
+			return 8;
+		} else if (isFullHouse()){
+			return 7;
+		} else if (isFlush()){
+			return 6;
+		} else if (isStraight()){
+			return 5;
+		} else if (isTriple()){
+			return 4;
+		} else if (isTwoPair()){
+			return 3;
+		} else if (isOnePair()){
+			return 2;
+		} else { //high card
+			return 1;
+		}
+	}
+
+	public boolean isStraight(){
+		this.organizeHand();
+		if (h.get(0).getRankValue() == 14 && h.get(1).getRankValue() == 2 && h.get(2).getRankValue() == 3 && h.get(3).getRankValue() == 4 && h.get(4).getRankValue() == 5) {
+			return true;
+		}
+		if (h.get(4).getRankValue() < 5) {
+			return false;
+		}
+		for (int i = 0; i < 4; i++) {
+			if (!(h.get(i).getRankValue() + 1 == h.get(i+1).getRankValue())) {
+				return false;	
+			}
+		}
+		return true;
+	}
+
+	public boolean isFlush(){
+		int first = h.get(0).getSuitValue();
+		for (Card c : h) {
+			if (c.getSuitValue() != first) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean isFourOfKind(){
+		int firstVal = h.get(0).getRankValue();
+		int secondVal = h.get(1).getRankValue();
+		boolean flag = true;
+		//check first four
+		for (int i = 1; i < 3; i++) {
+			if (h.get(i).getRankValue() != firstVal) {
+				flag = false;
+				break;
+			}
+		}
+		//check second four if first four weren't same
+		if (flag == false) {
+			for (int i = 2; i < 4; i++) {
+				if (h.get(i).getRankValue() != secondVal) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return flag;
+	}
+
+	//returns True hand wins, False if other hand wins
+	public boolean versus(Hand other){
+		if(this.handValue() > other.handValue()){
+			return true;
+		} else if (this.handValue() < other.handValue()){
+			return false;
+		} else {
+			/*
+			have to deal with kicker
+			*/
+		}
+	}
 }
