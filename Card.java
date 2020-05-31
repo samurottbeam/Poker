7b50d88a63dd20b1d5b80f7c68ed36fdd63ef3d2
Austin2Shih <66186160+Austin2Shih@users.noreply.github.com>
2020-05-30 18:43:51 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,14 +1,18 @@
 import java.awt.*;
 
-public class Card{
+public class Card implements Comparable<Card>{
 
 	private String rank;
 	private String suit;
-	
+	private int suitValue;
+	private int rankValue;
+
 	//constructor
-	public Card(String rank, String suit){
+	public Card(String rank, String suit, int suitValue, int rankValue){
 		this.rank = rank;
 		this.suit = suit;
+		this.suitValue = suitValue;
+		this.rankValue = rankValue;
 	}
 	//change rank
 	public void setRank(String newRank){
@@ -25,6 +29,33 @@ public class Card{
 
 	public String getSuit(){
 		return suit;
+	}
+
+	public int getSuitValue(){
+		return suitValue;
+	}
+
+	public int getRankValue(){
+		return rankValue;
+	}
+
+	public int compareTo(Card other){
+		if(this.getRank() == other.getRank()){
+			if(this.getSuitValue() > other.getSuitValue()){
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		}
+		else{
+			if(this.getRankValue() > other.getRankValue()){
+				return 1;
+			}
+			else{
+				return -1;
+			}
+		}
 	}
 
 }
