5e60a876131728cc470124057a3c27b07f4fad3a
jason-vu <66186269+jason-vu@users.noreply.github.com>
2020-05-31 00:28:26 -0700

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -12,8 +12,34 @@ public class Bot{
 		return h;
 	}
 		
-	public void Decision(){
-		
+	//bots course of action on swapping
+	public void decision(){
+		if (h.handValue() >= 5) {
+			return;
+		} else if (h.isAlmostFlush().size() != 0){
+			//swap other 2 cards
+			System.out.println(h.isAlmostFlush());
+			for (int i = 0; i < h.isAlmostFlush().size(); i++) {
+				h.swapCards(h.isAlmostFlush().get(i) + 1);
+			}
+		} else if (h.isAlmostStraight() > -1){
+			//swap last card
+			h.swapCards(h.isAlmostStraight() + 1);
+		} else if (h.moreThanTriple() > -1){
+			//swap card at h.moreThanTriple()
+			System.out.println(h.moreThanTriple());
+			h.swapCards(h.moreThanTriple() + 1);
+		} else if (h.moreThanOnePair().size() != 0){
+			//swap out other three
+			for (int i = 0; i < 3; i++) {
+				h.swapCards(h.moreThanOnePair().get(i) + 1);
+			}
+		} else {
+			//swap everything
+			for (int i = 0; i < 5; i++) {
+				h.swapCards(i + 1);
+			}
+		}
 	}
 
 }
