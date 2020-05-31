670d40a3a657126bbf7369ef20c155c2739d9a49
Austin2Shih <66186160+Austin2Shih@users.noreply.github.com>
2020-05-31 16:30:25 -0700

    Update Pot.java

--- Pot.java
+++ Pot.java
@@ -1,1 +1,37 @@
-Money
+public class Pot{
+	private int money;
+	private int currBid;
+
+	public Pot(){
+		money = 0;
+		currBid = 0;
+	}
+
+	public void Call(Player p){
+		p.changeMoney(-currBid);
+		money += currBid;
+	}
+
+	public void Raise(Player p, int bid){
+		if(bid>=currBid){
+			p.changeMoney(-bid);
+			money += bid;
+			currBid = bid;
+		}
+		else{
+			System.out.println("You must bet at least $" + currBid + ".");
+		}
+	}
+
+	public void Fold(Player p){
+		//remove from the round
+	}
+
+	public int getMoney(){
+		return money;
+	}
+
+	public int getCurrBid(){
+		return currBid;
+	}
+}
