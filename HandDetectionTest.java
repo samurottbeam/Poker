66b6476f08a0b87b154b79a669ef9a6b7fa1cd5d
jason-vu <66186269+jason-vu@users.noreply.github.com>
2020-06-01 00:35:28 -0700

    Update HandDetectionTest.java

--- HandDetectionTest.java
+++ HandDetectionTest.java
@@ -11,13 +11,22 @@ public class HandDetectionTest{
 			initial.add(new Card(console.nextInt(), console.nextInt()));
 		}
 		Hand hand = new Hand(d, initial);
-		Bot bot = new Bot(hand);
-		System.out.println(hand.handValue());
+		Bot bot = new Bot("Dealer", hand);
 
+		
+		System.out.println();
+		System.out.println(bot.getHand());
+		System.out.println(bot.getHand().getHandValue());
+		System.out.println("I have a first round confidence level of " + bot.getConfidenceLevel(1));
+		System.out.println(bot.getHand().getHandValue());
+		System.out.println("I have a second round confidence level of " + bot.getConfidenceLevel(2));
+
+		/*
 		bot.decision();
 		System.out.println();
-		
 		System.out.println(bot.getHand());
 		System.out.println(bot.getHand().handValue());
+		System.out.println("I have a confidence level of " + bot.getConfidenceLevel(2));
+		*/
 	}
 }
